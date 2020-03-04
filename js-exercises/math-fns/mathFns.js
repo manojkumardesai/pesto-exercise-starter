fdca3110a13b13c530dbd3a03743b0de1428fc41
Manoj Kumar Desai <manoj.kumar.desai@in.verizon.com>
2020-03-04 19:00:11 +0530

    Solution to Math Fns assignments.

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,21 @@
+
+// Don't change the export values.
+// You can use the function in the Math module
+
+function sqrt(number) {
+  return Math.sqrt(number);
+}
+
+function power(baseNumber, toThePowerOff) {
+  return Math.pow(baseNumber, toThePowerOff);
+}
+
+function round(numberToBeRounded) {
+  return Math.round(numberToBeRounded);
+}
+
+export {
+  sqrt,
+  power,
+  round,
+};
